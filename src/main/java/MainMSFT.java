3d5daa927c4353cffaea31e650ac66c7c00281b2
Nacho Garcia Segovia <nacho.gs@zaleos.net>
2021-04-30 00:47:05 +0200

    WIP

--- src/main/java/MainMSFT.java
+++ src/main/java/MainMSFT.java
@@ -1,52 +1,19 @@
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.math3.ml.neuralnet.MapUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.datavec.api.records.reader.RecordReader;
-import org.datavec.api.records.reader.SequenceRecordReader;
-import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVSequenceRecordReader;
-import org.datavec.api.records.writer.RecordWriter;
-import org.datavec.api.records.writer.impl.csv.CSVRecordWriter;
-import org.datavec.api.split.FileSplit;
-import org.datavec.api.split.NumberedFileInputSplit;
-import org.datavec.api.split.partition.NumberOfRecordsPartitioner;
-import org.datavec.api.split.partition.Partitioner;
-import org.datavec.api.transform.Transform;
 import org.datavec.api.transform.TransformProcess;
 import org.datavec.api.transform.schema.Schema;
-import org.datavec.api.util.ClassPathResource;
 import org.datavec.api.writable.Writable;
 import org.datavec.spark.transform.SparkTransformExecutor;
 import org.datavec.spark.transform.misc.StringToWritablesFunction;
 import org.datavec.spark.transform.misc.WritablesToStringFunction;
-import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
-import org.deeplearning4j.datasets.datavec.SequenceRecordReaderDataSetIterator;
-import org.deeplearning4j.datasets.iterator.IteratorDataSetIterator;
-import org.deeplearning4j.eval.Evaluation;
-import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
-import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.conf.layers.DenseLayer;
-import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.deeplearning4j.nn.weights.WeightInit;
 import org.joda.time.DateTimeZone;
-import org.nd4j.linalg.activations.Activation;
-import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.dataset.DataSet;
-import org.nd4j.linalg.dataset.SplitTestAndTrain;
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
-import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
-import org.nd4j.linalg.dataset.api.preprocessor.NormalizerStandardize;
-import org.nd4j.linalg.lossfunctions.LossFunctions;
-import org.datavec.local.transforms.LocalTransformExecutor;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.*;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class MainMSFT {
 
@@ -64,10 +31,8 @@ public class MainMSFT {
         int NUM_LABEL_CLASSES = 2;
 
 
-
         int numLinesToSkip = 1;
         String delimiter = ",";
-
 
 
         //=====================================================================
@@ -82,9 +47,7 @@ public class MainMSFT {
                 //Or for convenience define multiple columns of the same type
                 .addColumnsString("Cerrar")
                 //We can define different column types for different types of data:
-                .addColumnInteger("Volumen")
-                .addColumnsString("Abrir", "Alto", "Bajo")
-                .build();
+                .addColumnInteger("Volumen").addColumnsString("Abrir", "Alto", "Bajo").build();
 
 
         //Print out the schema:
@@ -109,25 +72,27 @@ public class MainMSFT {
         //At each step, we identify column by the name we gave them in the input data schema, above
         TransformProcess tp = new TransformProcess.Builder(inputDataSchema)
                 //Let's remove some column we don't need
-                .removeColumns("Cerrar","Abrir","Bajo")
+                .removeColumns("Cerrar", "Abrir", "Bajo")
 
-                //Let's suppose our data source isn't perfect, and we have some invalid data: negative dollar amounts that we want to replace with 0.0
+                //Let's suppose our data source isn't perfect, and we have some invalid data: negative dollar amounts
+                // that we want to replace with 0.0
                 //For positive dollar amounts, we don't want to modify those values
                 //Use the ConditionalReplaceValueTransform on the "TransactionAmountUSD" column:
-                .replaceStringTransform("Alto", transform)
-                .convertToDouble("Alto")
+                .replaceStringTransform("Alto", transform).convertToDouble("Alto")
 
                 //Finally, let's suppose we want to parse our date/time column in a format like "2016/01/01 17:50.000"
-                //We use JodaTime internally, so formats can be specified as follows: http://www.joda.org/joda-time/apidocs/org/joda/time/format/DateTimeFormat.html
+                //We use JodaTime internally, so formats can be specified as follows: http://www.joda
+                // .org/joda-time/apidocs/org/joda/time/format/DateTimeFormat.html
                 .stringToTimeTransform("Fecha", "MM/DD/YYYY", DateTimeZone.UTC)
 
-                //We've finished with the sequence of operations we want to do: let's create the final TransformProcess object
+                //We've finished with the sequence of operations we want to do: let's create the final
+                // TransformProcess object
                 .build();
 
         //Now, print the schema after each time step:
         int numActions = tp.getActionList().size();
 
-        for(int i=0; i<numActions; i++ ){
+        for (int i = 0; i < numActions; i++) {
             System.out.println("\n\n==================================================");
             System.out.println("-- Schema after step " + i + " (" + tp.getActionList().get(i) + ") --");
 
@@ -147,8 +112,7 @@ public class MainMSFT {
         //=====================================================================
 
 
-
-        String baseDir = "/Volumes/data/work/zaleos/git/Deeplearning4jTutorial/src/main/resources/";
+        String baseDir = "/Volumes/data/work/zaleos/git/CoronaTrade/src/main/resources/";
         String fileName = "HistoricalData_1614354695394.csv";
         String inputPath = baseDir + fileName;
         String timeStamp = String.valueOf(new Date().getTime());
@@ -171,60 +135,15 @@ public class MainMSFT {
         // convert to Writable
         JavaRDD<List<Writable>> MSFTReports = lines.map(new StringToWritablesFunction(rr));
         // run our transform process
-        JavaRDD<List<Writable>> processed = SparkTransformExecutor.execute(MSFTReports,tp);
+        JavaRDD<List<Writable>> processed = SparkTransformExecutor.execute(MSFTReports, tp);
         // convert Writable back to string for export
-        JavaRDD<String> toSave= processed.map(new WritablesToStringFunction(","));
+        JavaRDD<String> toSave = processed.map(new WritablesToStringFunction(","));
 
         toSave.saveAsTextFile(outputPath);
 
         long total = processed.count();
         long train_size = (long) (0.7 * total);
         List<List<Writable>> train = processed.takeOrdered((int) train_size);
-
-
-        //=====================================================================
-        //      Step 4:
-        //=====================================================================
-
-        SequenceRecordReaderDataSetIterator trainData = new SequenceRecordReaderDataSetIterator(processed, trainLabels,
-                                                                32, 2, false, SequenceRecordReaderDataSetIterator.AlignmentMode.ALIGN_END)
-
-
-        // training data
-        SequenceRecordReader trainRR = new CSVSequenceRecordReader(0, ", ");
-        trainRR.initialize(new NumberedFileInputSplit(outputPath + "/%d.csv", 0, 449));
-        SequenceRecordReaderDataSetIterator trainIter = new SequenceRecordReaderDataSetIterator(trainRR, BATCH_SIZE, NUM_LABEL_CLASSES, 1);
-
-        // testing data
-        SequenceRecordReader testRR = new CSVSequenceRecordReader(0, ", ");
-        testRR.initialize(new NumberedFileInputSplit(outputPath + "/%d.csv", 450, 599));
-        SequenceRecordReaderDataSetIterator testIter = new SequenceRecordReaderDataSetIterator(testRR, BATCH_SIZE, NUM_LABEL_CLASSES, 1);
-
-
-        int miniBatchSize = 10;
-        int numLabelClasses = 6;
-        DataSetIterator trainData2 = new SequenceRecordReaderDataSetIterator(trainRR, null, miniBatchSize, numLabelClasses,
-                                                                            false, SequenceRecordReaderDataSetIterator.AlignmentMode.ALIGN_END);
-
-        //Normalize the training data
-        DataNormalization normalizer = new NormalizerStandardize();
-        normalizer.fit(trainData);              //Collect training data statistics
-        trainData.reset();
-
-        DataSetIterator trainIter = new RecordReaderDataSetIterator(rr, 30, 0, 2);
-        List<List<Writable>> processedData2 = LocalTransformExecutor.execute(trainIter., tp);
-
-        DataSet allData = trainIter.next();
-
-        SplitTestAndTrain testAndTrain = allData.splitTestAndTrain(0.70);
-        DataSet trainingData = testAndTrain.getTrain();
-        DataSet testData = testAndTrain.getTest();
-
-
-        DataSetIterator iterClassification = new SequenceRecordReaderDataSetIterator(rr, 30, 0, 1);
-
-
-
 
 
         System.out.println("\n\nDONE");
